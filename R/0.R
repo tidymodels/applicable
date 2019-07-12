ce0d567f9b931d84c7f54062c398c7aacedbfacf
Marly Cormar <marlycormar@ufl.edu>
2019-07-12 11:19:34 -0400

    Export score function and update documentation accordingly.

--- R/0.R
+++ R/0.R
@@ -1,8 +1,12 @@
+#' A scoring function
+#' @export
 score <- function (object, ...) {
   UseMethod("score")
 }
 
-#
+#' @export
+#' @export score.default
+#' @rdname score
 score.default <- function(object, ...) {
   stop("`score()` is not implemented for an object of this class.", call. = FALSE)
 }
