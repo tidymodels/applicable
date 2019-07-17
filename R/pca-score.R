35c73a5ed30016db8a95911d8a83c425826ecf8c
Marly Cormar <marlycormar@ufl.edu>
2019-07-17 06:14:40 -0400

    Use predict function in the score implementation method.

--- R/pca-score.R
+++ R/pca-score.R
@@ -2,8 +2,7 @@
 # ----------------- Model function implementation -------------------
 # -------------------------------------------------------------------
 score_ad_pca_numeric <- function(model, predictors) {
-  predictions <- rep(1L, times = nrow(predictors))
-  hardhat::spruce_numeric(predictions)
+  stats::predict(model$pcs, predictors)
 }
 
 # -------------------------------------------------------------------
