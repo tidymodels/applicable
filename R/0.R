6a19b5488e940dc80ee7b67609c584e8e75ba7b7
Marly Cormar <marlycormar@ufl.edu>
2019-07-18 11:22:13 -0400

    Document score function arguments to remove warning.

--- R/0.R
+++ R/0.R
@@ -1,4 +1,14 @@
 #' A scoring function
+#'
+#' @param object Depending on the context:
+#'
+#'   * A __data frame__ of predictors.
+#'   * A __matrix__ of predictors.
+#'   * A __recipe__ specifying a set of preprocessing steps
+#'     created from [recipes::recipe()].
+#'
+#' @param ... Not currently used, but required for extensibility.
+#'
 #' @export
 score <- function (object, ...) {
   UseMethod("score")
