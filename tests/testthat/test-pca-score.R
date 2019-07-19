33a00e23504d4b6271497e1ab99ed43a083079a2
Marly Cormar <marlycormar@ufl.edu>
2019-07-19 12:17:01 -0400

    Test score function for predictors of type vectors.

--- tests/testthat/test-pca-score.R
+++ tests/testthat/test-pca-score.R
@@ -7,3 +7,12 @@ test_that("`score` fails when predictors are factors", {
     "The class of `new_data`, 'factor', is not recognized."
   )
 })
+
+test_that("`ad_pca` fails when predictors are vectors", {
+  skip("Skipping until I understand why the expected & actual message differ.")
+
+  expect_error(
+    ad_pca(iris$Sepal.Length),
+    "`ad_pca()` is not defined for a 'numeric'."
+    )
+})
