0e6eaf83ed741429ab0fe83dcd12b173cfd71fc2
Marly Cormar <marlycormar@ufl.edu>
2019-07-21 14:10:34 -0400

    Move `score` definition to pca-fit.R.

--- R/pca-score.R
+++ R/pca-score.R
@@ -69,3 +69,29 @@ score.ad_pca <- function(object, new_data, type = "numeric", ...) {
 valid_predict_types <- function() {
   c("numeric")
 }
+
+# -------------------------------------------------------------------
+# -------------------- Generic score function -----------------------
+# -------------------------------------------------------------------
+#' A scoring function
+#'
+#' @param object Depending on the context:
+#'
+#'   * A __data frame__ of predictors.
+#'   * A __matrix__ of predictors.
+#'   * A __recipe__ specifying a set of preprocessing steps
+#'     created from [recipes::recipe()].
+#'
+#' @param ... Not currently used, but required for extensibility.
+#'
+#' @export
+score <- function (object, ...) {
+  UseMethod("score")
+}
+
+#' @export
+#' @export score.default
+#' @rdname score
+score.default <- function(object, ...) {
+  rlang::abort(message = "`score()` is not implemented for an object of this class.")
+}
