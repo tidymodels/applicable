3d95a6ce1f4e62f814f571aab174866382a28ab8
Marly Cormar <marlycormar@ufl.edu>
2019-07-24 14:14:05 -0400

    Convert `score` output to tibble.

--- R/pca-score.R
+++ R/pca-score.R
@@ -4,7 +4,9 @@
 score_ad_pca_numeric <- function(model, predictors) {
   if(!("pcs" %in% names(model)))
     rlang::abort("The model must contain a pcs argument.")
-  stats::predict(model$pcs, predictors)
+  predicted_output <- stats::predict(model$pcs, predictors)
+  predicted_output <- tibble::as_tibble(predicted_output)
+  predicted_output
   # add the distance column
   # notes:
   # te <- score(mod, test)
