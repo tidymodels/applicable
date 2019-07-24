f9203522f9f3b7d7c76943181fdf8342ca1f259b
Marly Cormar <marlycormar@ufl.edu>
2019-07-24 13:40:50 -0400

    Update print.ad_pca function.

--- R/print.R
+++ R/print.R
@@ -1,17 +1,23 @@
 #' @export
 print.ad_pca <- function(x, ...) {
-  #cat_line("something here")
   eigs <- x$pcs$sdev^2
   cum_sum <- cumsum(eigs)/sum(eigs)
   ninety_five_prop_var <- sum(cum_sum <= 95) + 1
+  predictors_count <- ncol(x$blueprint$ptypes$predictors)
 
-  print(
-    paste("The first",
-           ninety_five_prop_var,
-           "principal components account for 95%",
-           "of the total variation in the predictors.")
-        )
-  print(x$pcs$rotation)
+  cat(
+    "# Predictors:",
+    predictors_count
+  )
+
+  cat("\n")
+
+  cat("# Principal Components:",
+      "The first",
+      ninety_five_prop_var,
+      "principal components account for 95% \n",
+      "of the total variation in the predictors."
+    )
 
   invisible(x)
 }
