81f3b2b1c46a869215dbcdbf791f2f2bd795743c
Marly Cormar <marlycormar@ufl.edu>
2019-07-29 17:41:34 -0400

    Move hat values calculations to separate R files.

---
+++ R/hat_values-fit.R
@@ -0,0 +1,151 @@
+# -----------------------------------------------------------------------------
+# ---------------------- Model Constructor ------------------------------------
+# -----------------------------------------------------------------------------
+
+new_apd_hat_values <- function(XtX_inv, blueprint) {
+  hardhat::new_model(
+    XtX_inv = XtX_inv,
+    blueprint = blueprint,
+    class = "apd_hat_values"
+  )
+}
+
+# -----------------------------------------------------------------------------
+# ---------------------- Model function implementation ------------------------
+# -----------------------------------------------------------------------------
+
+apd_hat_values_impl <- function(predictors) {
+  res <-
+    list(
+      XtX_inv = round(get_inv(predictors), 3)
+    )
+
+  res
+}
+
+get_inv <- function(X) {
+  if (!is.matrix(X)) {
+    X <- as.matrix(X)
+  }
+
+  XpX <- t(X) %*% X
+  XpX_inv <- try(qr.solve(XpX), silent = TRUE)
+
+  if (inherits(XpX_inv, "try-error")) {
+    rlang::abort(message = as.character(XpX_inv))
+  }
+
+  dimnames(XpX_inv) <- NULL
+  XpX_inv
+}
+
+# -----------------------------------------------------------------------------
+# ------------------------ Model function bridge ------------------------------
+# -----------------------------------------------------------------------------
+
+apd_hat_values_bridge <- function(processed, ...) {
+  predictors <- processed$predictors
+
+  fit <- apd_hat_values_impl(predictors)
+
+  new_apd_hat_values(
+    XtX_inv = fit$XtX_inv,
+    blueprint = processed$blueprint
+  )
+}
+
+# -----------------------------------------------------------------------------
+# ----------------------- Model function interface ----------------------------
+# -----------------------------------------------------------------------------
+
+#' Fit a `apd_hat_values`
+#'
+#' `apd_hat_values()` fits a model.
+#'
+#' @param x Depending on the context:
+#'
+#'   * A __data frame__ of predictors.
+#'   * A __matrix__ of predictors.
+#'   * A __recipe__ specifying a set of preprocessing steps
+#'     created from [recipes::recipe()].
+#'
+#' @param data When a __recipe__ or __formula__ is used, `data` is specified as:
+#'
+#'   * A __data frame__ containing the predictors.
+#'
+#' @param formula A formula specifying the predictor terms on the right-hand
+#' side. No outcome should be specified.
+#'
+#' @param ... Not currently used, but required for extensibility.
+#'
+#' @return
+#'
+#' A `apd_hat_values` object.
+#'
+#' @examples
+#' predictors <- mtcars[, -1]
+#'
+#' # Data frame interface
+#' mod <- apd_hat_values(predictors)
+#'
+#' # Formula interface
+#' mod2 <- apd_hat_values(mpg ~ ., mtcars)
+#'
+#' # Recipes interface
+#' library(recipes)
+#' rec <- recipe(mpg ~ ., mtcars)
+#' rec <- step_log(rec, disp)
+#' mod3 <- apd_hat_values(rec, mtcars)
+#'
+#' @export
+apd_hat_values <- function(x, ...) {
+  UseMethod("apd_hat_values")
+}
+
+#' @export
+#' @rdname apd_hat_values
+apd_hat_values.default <- function(x, ...) {
+  cls <- class(x)[1]
+  message <-
+    "`x` is not of a recognized type.
+     Only data.frame, matrix, recipe, and formula objects are allowed.
+     A {cls} was specified."
+  message <- glue::glue(message)
+  rlang::abort(message = message)
+}
+
+# Data frame method
+
+#' @export
+#' @rdname apd_hat_values
+apd_hat_values.data.frame <- function(x, ...) {
+  processed <- hardhat::mold(x, NA_real_)
+  apd_hat_values_bridge(processed, ...)
+}
+
+# Matrix method
+
+#' @export
+#' @rdname apd_hat_values
+apd_hat_values.matrix <- function(x, ...) {
+  processed <- hardhat::mold(x, NA_real_)
+  apd_hat_values_bridge(processed, ...)
+}
+
+# Formula method
+
+#' @export
+#' @rdname apd_hat_values
+apd_hat_values.formula <- function(formula, data, ...) {
+  processed <- hardhat::mold(formula, data)
+  apd_hat_values_bridge(processed, ...)
+}
+
+# Recipe method
+
+#' @export
+#' @rdname apd_hat_values
+apd_hat_values.recipe <- function(x, data, ...) {
+  processed <- hardhat::mold(x, data)
+  apd_hat_values_bridge(processed, ...)
+}
---
+++ R/hat_values-score.R
@@ -0,0 +1,82 @@
+# -----------------------------------------------------------------------------
+# ---------------------- Model function implementation ------------------------
+# -----------------------------------------------------------------------------
+
+score_apd_hat_values_numeric <- function(model, predictors) {
+  if(!("XtX_inv" %in% names(model)))
+    rlang::abort("The model must contain a XtX_inv argument.")
+
+  proj_matrix <- predictors %*% model$XtX_inv %*% t(predictors)
+  hat_values <- diag(proj_matrix)
+
+  tibble::as_tibble(
+    cbind(
+      hat_values
+    )
+  )
+}
+
+# -----------------------------------------------------------------------------
+# ------------------------ Model function bridge ------------------------------
+# -----------------------------------------------------------------------------
+
+score_apd_hat_values_bridge <- function(type, model, predictors) {
+  predictors <- as.matrix(predictors)
+
+  score_function <- get_hat_values_score_function(type)
+  predictions <- as.data.frame(score_function(model, predictors))
+
+  hardhat::validate_prediction_size(predictions, predictors)
+
+  predictions
+}
+
+get_hat_values_score_function <- function(type) {
+  switch(
+    type,
+    numeric = score_apd_hat_values_numeric
+  )
+}
+
+# -----------------------------------------------------------------------------
+# ----------------------- Model function interface ----------------------------
+# -----------------------------------------------------------------------------
+
+#' Predict from a `apd_hat_values`
+#'
+#' @param object A `apd_hat_values` object.
+#'
+#' @param new_data A data frame or matrix of new predictors.
+#'
+#' @param type A single character. The type of predictions to generate.
+#' Valid options are:
+#'
+#' - `"numeric"` for numeric predictions.
+#'
+#' @param ... Not used, but required for extensibility.
+#'
+#' @return
+#'
+#' A tibble of predictions. The number of rows in the tibble is guaranteed
+#' to be the same as the number of rows in `new_data`.
+#'
+#' @examples
+#' train <- mtcars[1:20,]
+#' test <- mtcars[21:32, -1]
+#'
+#' # Fit
+#' mod <- apd_hat_values(mpg ~ cyl + log(drat), train)
+#'
+#' # Predict, with preprocessing
+#' score(mod, test)
+#'
+#' @export
+score.apd_hat_values <- function(object, new_data, type = "numeric", ...) {
+  forged <- hardhat::forge(new_data, object$blueprint)
+  rlang::arg_match(type, valid_predict_types())
+  score_apd_hat_values_bridge(type, object, forged$predictors)
+}
+
+valid_predict_types <- function() {
+  c("numeric")
+}
