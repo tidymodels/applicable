d2e6d324c7451655d6666814c35230d7571718f8
topepo <mxkuhn@gmail.com>
2019-08-06 13:02:03 -0400

    Similarity reference

--- R/similarity.R
+++ R/similarity.R
@@ -133,7 +133,7 @@ apd_similarity_bridge <- function(processed, quantile = NA_real_, ...) {
 #' @details The function computes measures of similarity for different samples
 #'  points. For example, suppose samples `A` and `B` both contain _p_ binary
 #'  variables. First, a 2x2 table is constructed between `A` and `B` _across
-#'  their elements. The table will contain _p_ entires across the four cells
+#'  their elements_. The table will contain _p_ entires across the four cells
 #'  (see the example below). From this, different measures of likeness are
 #'  computed.
 #'
@@ -152,6 +152,8 @@ apd_similarity_bridge <- function(processed, quantile = NA_real_, ...) {
 #'
 #' A `apd_similarity` object.
 #'
+#' @references Leach, A. and Gillet V. (2007). _An Introduction to
+#' Chemoinformatics_. Springer, New York
 #' @examples
 #' \dontrun{
 #' data(qsar_binary)
