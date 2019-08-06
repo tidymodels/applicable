b37e086d1bd0802cda83fc4bd53062caa9d8aff2
Marly Cormar <marlycormar@ufl.edu>
2019-08-06 15:13:17 -0400

    Correct wording on `print.apd_pca`.

--- R/print.R
+++ R/print.R
@@ -3,14 +3,18 @@ print.apd_pca <- function(x, ...) {
   predictors_count <- ncol(x$blueprint$ptypes$predictors)
   percentage <- x$threshold * 100
   num_comp <- x$num_comp
+  wording <- "components were"
+
+  if(num_comp == 1)
+    wording <- "component was"
 
   print_output <- glue::glue(
   "# Predictors:
       {predictors_count}
    # Principal Components:
-      The first {num_comp} principal components
-      account for {percentage}% of the total variation
-      in the predictors."
+      {num_comp} {wording} needed
+      to capture at least {percentage}% of the
+      total variation in the predictors."
   )
 
  cat(print_output)
