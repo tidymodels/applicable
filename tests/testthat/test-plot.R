169e5787edf3e4c0ba041ac201c1c5145345e1d4
Marly Cormar <marlycormar@ufl.edu>
2019-08-07 08:19:28 -0400

    Test `autoplot` when options are provided.

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,10 +1,34 @@
-library(ggplot2)
-
-test_that("output of autoplot.apd_pca is correct when no options is provided", {
+test_that("output of autoplot.apd_pca is correct when no options are provided", {
   ad <- apd_pca(mtcars)
-  ad_plot <- autoplot(ad)
+  ad_plot <- ggplot2::autoplot(ad)
 
   pctls <- ad$pctls %>%
+    tidyr::gather(component, value, -percentile)
+
+  expect_equal(ad_plot$data, pctls)
+  expect_equal(ad_plot$labels$x, "value")
+  expect_equal(ad_plot$labels$y, "percentile")
+})
+
+test_that("output of autoplot.apd_pca is correct when options=matches are provided", {
+  ad <- apd_pca(mtcars)
+  ad_plot <- ggplot2::autoplot(ad, matches("PC[1-5]"))
+
+  pctls <- ad$pctls %>%
+    select(matches("PC[1-5]"), percentile) %>%
+    tidyr::gather(component, value, -percentile)
+
+  expect_equal(ad_plot$data, pctls)
+  expect_equal(ad_plot$labels$x, "value")
+  expect_equal(ad_plot$labels$y, "percentile")
+})
+
+test_that("output of autoplot.apd_pca is correct when options=distance are provided", {
+  ad <- apd_pca(mtcars)
+  ad_plot <- ggplot2::autoplot(ad, "distance")
+
+  pctls <- ad$pctls %>%
+    select(matches("distance"), percentile) %>%
     tidyr::gather(component, value, -percentile)
 
   expect_equal(ad_plot$data, pctls)
