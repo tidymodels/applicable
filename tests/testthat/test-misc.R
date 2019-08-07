9fc3eb429734ac2cf72feb718b761c930990eb1b
Marly Cormar <marlycormar@ufl.edu>
2019-08-07 13:54:47 -0400

    Add unit test for `names0`.

---
+++ tests/testthat/test-misc.R
@@ -0,0 +1,8 @@
+test_that("`names0` fails if `num` is less than 1", {
+  num <- 0
+  expect_error(
+    names0(num),
+    message = "NA/NaN argument
+    In addition: Warning message:
+    In format(1:num) : NAs introduced by coercion")
+})
