75e1a78bc4ba6968c9c55bc9445c27df35101301
Marly Cormar <marlycormar@ufl.edu>
2019-09-02 23:42:52 -0400

    Include the footer.

---
+++ inst/shiny/templates/footer.R
@@ -0,0 +1,8 @@
+argonFooter <- argonDashFooter(
+  copyrights = "@RStudio, 2019",
+  src = "https://www.rstudio.com",
+  argonFooterMenu(
+    argonFooterItem("tidymodels", src = "https://github.com/tidymodels"),
+    argonFooterItem("applicable", src = "https://github.com/tidymodels/applicable")
+  )
+)
