153875aebb18420da178093920b87fd5273da0e1
Marly Cormar <marlycormar@ufl.edu>
2019-09-02 23:42:08 -0400

    Add shiny app.

---
+++ inst/shiny/app.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(argonR)
+library(argonDash)
+library(magrittr)
+
+# templates
+source("templates/sidebar.R")
+#source("templates/navbar.R")
+source("templates/header.R")
+source("templates/footer.R")
+
+# elements
+source("elements/upload_tab.R")
+source("elements/models_tab.R")
+source("elements/help_tab.R")
+
+
+# App
+shiny::shinyApp(
+  ui = argonDashPage(
+    title = "Applicability Domain Methods",
+    author = "Marly Cormar & Max Kuhn",
+    description = "Applicability Domain Methods Using `applicable`",
+    sidebar = argonSidebar,
+    #navbar = argonNav,
+    header = argonHeader,
+    body = argonDashBody(
+      argonTabItems(
+        upload_tab,
+        models_tab,
+        help_tab
+      )
+    ),
+    footer = argonFooter
+  ),
+  server = function(input, output) {
+    output$distPlot <- renderPlot({
+      hist(rnorm(input$obs))
+    })
+
+    output$plot <- renderPlot({
+      dist <- switch(
+        input$dist,
+        norm = rnorm,
+        unif = runif,
+        lnorm = rlnorm,
+        exp = rexp,
+        rnorm
+      )
+
+      hist(dist(500))
+    })
+  }
+)
