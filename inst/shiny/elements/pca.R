5866ce480defb9c11f70857b572fc9c5692cb3d7
Marly Cormar <marlycormar@ufl.edu>
2019-09-02 23:46:04 -0400

    Add template for pca tab.

---
+++ inst/shiny/elements/pca.R
@@ -0,0 +1,8 @@
+tabPCA <- "There are times when a model’s prediction should be taken with some
+skepticism. For example, if a new data point is substantially different
+from the training set, it’s predicted value may be suspect. In
+chemistry, it is not uncommon to create an “applicability domain” model
+that measures the amount of potential extrapolation new samples have
+from the training set. applicable contains different methods to measure
+how much a new data point is an extrapolation from the original data (if
+at all)."
