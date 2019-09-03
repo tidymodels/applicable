df6f2cee6d6aa933a8fa9c8c3595a89022102920
Marly Cormar <marlycormar@ufl.edu>
2019-09-02 23:43:14 -0400

    Include header.

---
+++ inst/shiny/templates/header.R
@@ -0,0 +1,12 @@
+argonHeader <- argonDashHeader(
+  gradient = TRUE,
+  color = "primary",
+  separator = TRUE,
+  separator_color = "secondary",
+  argonH1(
+    display = 3,
+    "Applicability Domain Methods",
+    htmltools::span("with examples")
+  ) %>% argonTextColor(color = "white") #,
+  #argonLead("some info here") %>% argonTextColor(color = "white")
+)
