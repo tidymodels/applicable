02ea38a03a4ef27e0259037691fc69c539b98d62
Marly Cormar <marlycormar@ufl.edu>
2019-09-02 23:43:33 -0400

    Include sidebar.

---
+++ inst/shiny/templates/sidebar.R
@@ -0,0 +1,29 @@
+argonSidebar <- argonDashSidebar(
+  vertical = TRUE,
+  skin = "light",
+  background = "white",
+  size = "md",
+  side = "left",
+  id = "my_sidebar",
+  brand_url = "https://www.github.com/tidymodels/applicable",
+  brand_logo = "https://marlycormar.com/images/logo3.png",
+  argonSidebarHeader(title = "Main Menu"),
+  argonSidebarMenu(
+    argonSidebarItem(
+      tabName = "upload",
+      icon = argonIcon(name = "cloud-upload-96", color = "info"),
+      "Upload Data"
+    ),
+    argonSidebarItem(
+      tabName = "models",
+      icon = argonIcon(name = "atom", color = "warning"),
+      "Modeling Functions"
+    ),
+    argonSidebarItem(
+      tabName = "help",
+      icon = argonIcon(name = "books", color = "green"),
+      "Help"
+    )
+  ),
+  argonSidebarDivider()
+)
