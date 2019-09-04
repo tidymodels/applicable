39006c3444817056238cda2591ce7146059fce44
Marly Cormar <marlycormar@ufl.edu>
2019-09-04 13:48:01 -0400

    Update upload page on the shiny app.

--- inst/shiny/elements/upload_tab.R
+++ inst/shiny/elements/upload_tab.R
@@ -2,33 +2,29 @@ upload_tab <- argonTabItem(
   tabName = "upload",
 
   argonH1("Upload Data", display = 4),
+
   argonRow(
-    argonCard(
+    argonColumn(
       width = 12,
-      src = NULL,
-      icon = icon("cogs"),
-      status = "success",
-      shadow = TRUE,
-      border_level = 2,
-      hover_shadow = TRUE,
-      title = "Model Training Data",
-      fileInput("file1", "Choose a data file",
+      fileInput("train_data", "Choose a data file",
                 multiple = FALSE,
-                accept = c("text/csv",".csv")),
-      # Horizontal line ----
-      tags$hr(),
-      argonRow(
-        argonColumn(
-          width = 6,
-          sliderInput(
-            "obs",
-            "Number of observations:",
-            min = 0,
-            max = 1000,
-            value = 500
-          )
-        ),
-        argonColumn(width = 6, plotOutput("distPlot"))
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv"))
+    ),
+
+    argonRow(
+      argonCard(
+        width = 12,
+        src = NULL,
+        icon = icon("cogs"),
+        status = "success",
+        shadow = TRUE,
+        border_level = 2,
+        hover_shadow = TRUE,
+        title = "Model Training Data",
+        #argonColumn(tableOutput("dataSummary"))
+        argonColumn(dataTableOutput("dataOverview"))
       )
     ),
     br(), br()
