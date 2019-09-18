b0217a259138240b492d55aa458468d050319eb8
Marly Cormar <marlycormar@ufl.edu>
2019-09-18 06:30:41 -0400

    Change button to upload new samples after its enabled.

---
+++ inst/shiny/www/js/functions.js
@@ -0,0 +1,9 @@
+//window.alert('alksjdf');
+
+$(document).ready(function(){
+    $("#uploaded_train_data").on('change',function(){
+      $(".btn-default").css("background-color", "#5e72e4");
+      $(".btn-default").css("border-color", "#5e72e4");
+      $(".btn-default").css("color", "#fff");
+    });
+});
