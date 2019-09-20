85b35c376c0cd4f4b0f55ea9ca4f12b338150ba7
Marly Cormar <marlycormar@ufl.edu>
2019-09-20 11:38:54 -0400

    Update azure pipeline configuration and add proxyc to the imported packages.

--- tests/testthat.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-library(applicable)
-
-if (requireNamespace("xml2")) {
-  test_check("applicable", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("applicable")
-}
