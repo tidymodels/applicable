88769f22a2727eb951d54765840069eab1966064
Max Kuhn <mxkuhn@gmail.com>
2026-02-16 17:44:47 -0500

    2023 spring cleaning (#35)
    * pkgdown and GHA updates
    
    * usethis::use_tidy_style()
    
    * usethis::use_tidy_description()
    
    * regenerate snapshots with newer rlang
    
    * rstudio -> posit, fix urls
    
    * re-regenerate snapshots
    
    * move imports to package docs
    
    * udpate testthat and other pkg version
    
    * update imports
    
    * retrigger checks
    
    * update GHA
    
    ---------
    
    Co-authored-by: Marly Gotti <marlygotti@gmail.com>
    Co-authored-by: ‘topepo’ <‘mxkuhn@gmail.com’>

--- R/applicable-package.R
+++ R/applicable-package.R
@@ -1,8 +1,50 @@
 #' @keywords internal
 "_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+
+#' @import rlang
+#' @importFrom dplyr %>%
+#' @importFrom dplyr count
+#' @importFrom dplyr group_by
+#' @importFrom dplyr matches
+#' @importFrom dplyr mutate
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr rename_all
+#' @importFrom dplyr sample_n
+#' @importFrom dplyr select
+#' @importFrom dplyr slice
+#' @importFrom dplyr starts_with
+#' @importFrom dplyr ungroup
+#' @importFrom ggplot2 ggplot geom_step xlab ylab aes autoplot
+#' @importFrom glue glue
+#' @importFrom hardhat forge
+#' @importFrom hardhat mold
+#' @importFrom hardhat new_model
+#' @importFrom hardhat validate_prediction_size
+#' @importFrom Matrix Matrix colSums
+#' @importFrom proxyC simil
+#' @importFrom purrr map_dfc
+#' @importFrom purrr map2_dfc
+#' @importFrom stats approx
+#' @importFrom stats ecdf
+#' @importFrom stats prcomp
+#' @importFrom stats predict
+#' @importFrom stats quantile
+#' @importFrom stats setNames
+#' @importFrom tibble as_tibble
+#' @importFrom tibble tibble
+#' @importFrom tidyr gather
+#' @importFrom tidyselect vars_select
+#' @importFrom utils globalVariables
 ## usethis namespace: end
+
+# ------------------------------------------------------------------------------
+# global variable" check
+# nocov
+# nocov end
+# Reduce false positives when R CMD check runs its "no visible binding for
+utils::globalVariables(
+  c("cumulative", "n", "sim", "percentile", "component", "value")
+)
 NULL
--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -1,6 +1,7 @@
 test_that("`names0` fails if `num` is less than 1", {
   num <- 0
-  expect_snapshot(error = TRUE,
+  expect_snapshot(
+    error = TRUE,
     names0(num)
   )
 })
