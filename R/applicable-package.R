880fdb977fa00bd067e5ffc3359a7e5cf83db330
‘topepo’ <mxkuhn@gmail.com>
2026-02-16 21:23:10 -0500

    coolate with main

--- R/applicable-package.R
+++ R/applicable-package.R
@@ -1,56 +1,50 @@
 #' @keywords internal
 "_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-## usethis namespace: end
-NULL
 
+#' @import rlang
 #' @importFrom dplyr %>%
-#' @importFrom dplyr select
-#' @importFrom dplyr slice
+#' @importFrom dplyr count
+#' @importFrom dplyr group_by
 #' @importFrom dplyr matches
-#' @importFrom dplyr starts_with
-#' @importFrom dplyr rename_all
 #' @importFrom dplyr mutate
 #' @importFrom dplyr mutate_all
-#' @importFrom dplyr group_by
-#' @importFrom dplyr ungroup
-#' @importFrom dplyr count
+#' @importFrom dplyr rename_all
 #' @importFrom dplyr sample_n
+#' @importFrom dplyr select
+#' @importFrom dplyr slice
+#' @importFrom dplyr starts_with
+#' @importFrom dplyr ungroup
+#' @importFrom ggplot2 ggplot geom_step xlab ylab aes autoplot
 #' @importFrom glue glue
-#' @importFrom tibble as_tibble
-#' @importFrom tibble tibble
-#' @importFrom purrr map_dfc
-#' @importFrom purrr map2_dfc
-#' @importFrom rlang abort
-#' @importFrom rlang enquos
-#' @importFrom rlang arg_match
-#' @importFrom stats predict
-#' @importFrom stats prcomp
-#' @importFrom stats approx
-#' @importFrom stats quantile
-#' @importFrom stats ecdf
-#' @importFrom stats setNames
-#' @importFrom hardhat validate_prediction_size
 #' @importFrom hardhat forge
 #' @importFrom hardhat mold
 #' @importFrom hardhat new_model
-#' @importFrom ggplot2 ggplot geom_step xlab ylab aes autoplot
+#' @importFrom hardhat validate_prediction_size
 #' @importFrom Matrix Matrix colSums
-#' @importFrom tidyselect vars_select
-#' @importFrom tidyr gather
 #' @importFrom proxyC simil
+#' @importFrom purrr map_dfc
+#' @importFrom purrr map2_dfc
+#' @importFrom stats approx
+#' @importFrom stats ecdf
+#' @importFrom stats prcomp
+#' @importFrom stats predict
+#' @importFrom stats quantile
+#' @importFrom stats setNames
+#' @importFrom tibble as_tibble
+#' @importFrom tibble tibble
+#' @importFrom tidyr gather
+#' @importFrom tidyselect vars_select
+#' @importFrom utils globalVariables
+## usethis namespace: end
 
 # ------------------------------------------------------------------------------
-# nocov
-
-# Reduce false positives when R CMD check runs its "no visible binding for
 # global variable" check
-#' @importFrom utils globalVariables
+# nocov
+# nocov end
+# Reduce false positives when R CMD check runs its "no visible binding for
 utils::globalVariables(
   c("cumulative", "n", "sim", "percentile", "component", "value")
 )
-
-# nocov end
+NULL
